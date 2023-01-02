3b721449d1b5e2d9a2148d67488ee4e8a49486cb
erdenetsogt <a.erdenetsogt@gmail.com>
2023-01-02 17:01:15 +0800

    initial

---
+++ public/readme.txt
@@ -0,0 +1,27 @@
+Step 1 
+
+Install Nodejs
+
+https://nodejs.org/en/
+
+Check
+
+node -v
+npm -v
+
+Step 2 
+
+Install react 
+
+npx create-react-app [app-name]
+
+example
+
+npx create-react-app blog-web
+
+Step 3
+
+Run react
+
+cd blog-web
+npm start
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import Card from "./components/Card";
+
+export function Header() {
+    return (
+        <>
+            <Card />
+            <Card />
+            <Article />
+        </>
+    );
+}
+
+export function Article() {
+    return <div>hi</div>;
+}
