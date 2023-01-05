1c1c8b8d04e4e1db4ef686f9dcfa590d80496505
erdenetsogt <a.erdenetsogt@gmail.com>
2023-01-05 15:08:17 +0800

    use state

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { AwesomeButton } from "react-awesome-button";
 import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
@@ -6,24 +7,58 @@ import { categories } from "../mockdata/categories";
 const error = "";
 
 export function CategoryList({ deletable, editable }) {
+    const [counter, setCounter] = useState(10); //hook
+    const [editing, setEditing] = useState(); // ehnii utga
+    const [name, setName] = useState("hello");
+    const [dname, setDname] = useState("");
+
     if (error) {
         return null;
     }
+
+    console.log("rendering...");
 
     if (error) {
         return <div>{error}</div>;
     }
 
+    function increment(event) {
+        console.log(event);
+        if (true) {
+            setCounter(counter + 1);
+        }
+        setName("pinecone");
+    }
+
+    function handleChange(event) {
+        setName(event.target.value);
+    }
+    console.log(editing);
+
     return (
         <Container>
+            {counter}
+
+            {editing && (
+                <div>
+                    <input value={name} onChange={handleChange} />
+                    <button onClick={() => setEditing(false)}>close</button>
+                </div>
+            )}
+
+            {name}
+            <button onClick={increment}>Add</button>
+            <button onClick={() => setEditing(true)}>Шинэ</button>
             {categories.map((cat1) => (
-                <ListItem cat1={cat1} editable={cat1.editable} deletable={cat1.deletable} />
+                <ListItem name={name} cat1={cat1} editable={cat1.editable} deletable={cat1.deletable} />
             ))}
+            {name}
+            {name}
         </Container>
     );
 }
 
-function ListItem({ cat1, editable, deletable }) {
+function ListItem({ name, cat1, editable, deletable }) {
     function deleteThis() {
         alert("Delete");
     }
@@ -33,7 +68,7 @@ function ListItem({ cat1, editable, deletable }) {
             <Card.Body>
                 <div className="d-flex justify-content-between align-items-center">
                     <div>
-                        {cat1.name} {cat1.id}
+                        {cat1.name} {cat1.id} {name}
                     </div>
 
                     <div>
