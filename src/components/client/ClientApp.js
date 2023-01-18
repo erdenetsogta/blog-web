b4c9d4ad82958ebdba2fb3949d1abdf86e8a442a
erdenetsogt <a.erdenetsogt@gmail.com>
2023-01-18 17:35:48 +0800

    particles

--- src/components/client/ClientApp.js
+++ src/components/client/ClientApp.js
@@ -1,15 +1,19 @@
 import { Link, Route, Routes, useParams } from "react-router-dom";
+import { ParticlesSample } from "./ParticlesSample";
 
 export function ClientApp() {
     return (
         <div>
-            <Link to="/">Home</Link> <Link to="/blog">Blog</Link>
-            <Routes>
-                <Route path="/" element={<div>Home page</div>} />
-                <Route path="/blog" element={<div>Blog list</div>} />
-                <Route path="/blog/:slug" element={<SingleBlog />} />
-                <Route path="*" element={<NotFound />} />
-            </Routes>
+            <ParticlesSample />
+            <div style={{ position: "fixed" }}>
+                <Link to="/">Home</Link> <Link to="/blog">Blog</Link>
+                <Routes>
+                    <Route path="/" element={<div>Home page</div>} />
+                    <Route path="/blog" element={<div>Blog list</div>} />
+                    <Route path="/blog/:slug" element={<SingleBlog />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </div>
         </div>
     );
 }
